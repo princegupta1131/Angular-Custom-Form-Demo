774084008a9c8a75caf40d82b4e79f023971ec95
princegupta1131 <114015020+princegupta1131@users.noreply.github.com>
2023-03-10 16:08:20 +0530

    update readme and code cleanup

--- search-filter/src/app/app.component.ts
+++ search-filter/src/app/app.component.ts
@@ -12,13 +12,13 @@ export class AppComponent {
   formCreateData = {};
   formJsonData: any[] = [];
   formCreateJsonData: any[] = [];
-  languageJsonDatas = {}
+  languageJsonData = {}
   globalLangKey = '';
   langValue = '';// default selected language is required
   constructor(private http: HttpClient) { }
 
   filterTitleInfo = {
-    title: 'User Search Form :: ',
+    title: 'Sample User Search Form :: ',
     subTitle: 'User can search and filter the data using below form',
     submit: "Submit",
     clear: "Reset",
@@ -32,8 +32,8 @@ export class AppComponent {
 
   createFilterTitleInfo = {
     title: 'Json Creation :: ',
-    subTitle: 'Create a form json by using below form',
-    submit: "Create Form",
+    subTitle: 'Create a Sample JSON using below form library',
+    submit: "Generate JSON",
     clear: "Clear",
     styling: {
       orientation: "column",
@@ -43,98 +43,15 @@ export class AppComponent {
     isButtonReq: true
   }
 
-  languageJsonData = [{
-    en:
-    {
 
-      "Board": "Board",
-      "Medium": "Medium",
-      "Grade": "Grade",
-      "Subject": "Subject",
-      "Published user type": "Published user type",
-      "State": "State",
-      "Organistion": "Organistion",
-      "UserName": "UserName..",
-      "Parent": "Parent",
-      "Karnataka": "Karnataka",
-      "Filters matching your profile details": "Filters matching your profile details",
-      "School head OR Officials": "School head OR Officials",
-      "Student": "Student",
-      "Teacher": "Teacher",
-      "STATE-BR": "STATE-BR",
-      "Delhi": "Delhi",
-      "STATE-TN": "STATE-TN",
-      "Submit": "Submit",
-      "Reset": "Reset",
-      "Tamil Nadu": "Tamil Nadu",
-      "Bihar": "Bihar",
-      "Password": "Password"
-    },
-
-    hi: {
-
-      "Board": "बोर्ड",
-      "Medium": "माध्यम",
-      "Grade": "श्रेणी",
-      "Subject": "विषय",
-      "Published user type": "प्रकाशित उपयोगकर्ता के  प्रकार",
-      "State": "राज्य",
-      "organisation": "संगठन",
-      "UserName": "उपयोगकर्ता नाम",
-      "Search Filter": "फ़िल्टर खोजें",
-      "Parent": "माता-पिता",
-      "Karnataka": "कर्नाटक",
-      "Filters matching your profile details": "आपके प्रोफ़ाइल विवरण से मेल खाने वाले फ़िल्टर",
-      "School head OR Officials": "स्कूल प्रमुख या अधिकारी",
-      "Student": "विद्यार्थी",
-      "Teacher": "शिक्षक",
-      "STATE-BR": "राज्य-बीआर",
-      "Delhi": "दिल्ली",
-      "Submit": "प्रस्तुत करना",
-      "Reset": "रीसेट",
-      "Tamil Nadu": "तमिलनाडु",
-      "STATE-TN": "राज्य-तमिलनाडु",
-      "Bihar": "बिहार",
-      "Password": "पासवर्ड"
-
-    },
-    kn:
-    {
-      "Board": "ಮಂಡಳಿ/ವಿಶ್ವವಿದ್ಯಾಲಯ",
-      "Medium": "ಮಾಧ್ಯಮ",
-      "Grade": "ಗ್ರೇಡ್",
-      "Subject": "ವಿಷಯ",
-      "Published user type": "ಪ್ರಕಟಿತ ಬಳಕೆದಾರರ ಪ್ರಕಾರ",
-      "State": "ರಾಜ್ಯ",
-      "organisation": "ಸಂಸ್ಥೆ",
-      "UserName": "ಬಳಕೆದಾರ ಹೆಸರು",
-      "Search Filter": "ಹುಡುಕಾಟ ಫಿಲ್ಟರ್",
-      "Parent": "ಪೋಷಕ",
-      "Karnataka": "ಕರ್ನಾಟಕ",
-      "Filters matching your profile details": "ನಿಮ್ಮ ಪ್ರೊಫೈಲ್ ವಿವರಗಳಿಗೆ ಹೊಂದಾಣಿಕೆಯಾಗುವ ಫಿಲ್ಟರ್‌ಗಳು",
-      "School head OR Officials": "ಶಾಲಾ ಮುಖ್ಯಸ್ಥರು ಅಥವಾ ಅಧಿಕಾರಿಗಳು",
-      "Student": "ವಿದ್ಯಾರ್ಥಿ",
-      "Teacher": "ಶಿಕ್ಷಕ",
-      "STATE-BR": "ರಾಜ್ಯ",
-      "Delhi": "ದೆಹಲಿ",
-      "Submit": "ಸಲ್ಲಿಸು",
-      "Reset": "ಸ್ಪಷ್ಟ",
-      "Tamil Nadu": "ತಮಿಳುನಾಡು",
-      "STATE-TN": "ರಾಜ್ಯ",
-      "Bihar": "ಬಿಹಾರ",
-      "Password": "ಪಾಸ್ವರ್ಡ್"
-
-    }
-  }]
   onFilterChange($: any) {
     console.log('selcted Vaue ' + JSON.parse(JSON.stringify($)))
-    // alert('Success Submitted value is ' + JSON.stringify($))
     this.resUserData = JSON.parse(JSON.stringify($));
     console.log(this.resUserData)
   }
+
   getFilterData($: any) {
     console.log('selcted Created Vaue ' + JSON.parse(JSON.stringify($)))
-    // alert('Success Submitted value is ' + JSON.stringify($))
     console.log(this.resUserData);
     const formCreationData = {
       "index": parseInt($.data.index),
@@ -160,6 +77,7 @@ export class AppComponent {
     this.formJsonData.push(formCreationData);
     console.log('formjsonData', this.formJsonData);
   }
+  
   ngOnChanges() {
     this.ngOnInit();
   }
@@ -175,15 +93,12 @@ export class AppComponent {
       console.log(this.formCreateJsonData, + 'json');
     })
     this.http.get('./assets/languageJsonData.json').subscribe((formData: any) => {
-      this.languageJsonDatas = JSON.parse(JSON.stringify(formData));
+      this.languageJsonData = JSON.parse(JSON.stringify(formData));
       console.log(this.formJsonData, + 'json')
-      // this.resUserData = this.formJsonData; // input value
 
     })
   }
   onSelected($: any) {
     this.langValue = $.value;
-    // this.languageJsonDatas = this.languageJsonData.filterObject.keys(this.langValue)
-
   }
 }
